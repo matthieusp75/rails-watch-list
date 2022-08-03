0be31fa2c78fcd6d534effd81e08ea7efdada74b
Matthieu Seaux-Peters <matthieu.sp@gmail.com>
2022-08-03 10:46:18 +0200

    clean

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,5 +4,4 @@ class Bookmark < ApplicationRecord
 
   validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
-
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
-has_many :bookmarks
+  has_many :bookmarks
 
-validates :title, uniqueness: true, presence: true
-validates :overview, presence: true
-TITLE = Movie.all.order(:title)
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
+  TITLE = Movie.all.order(:title)
 end
